fa137cb72468c215ce9217b61a542e56d6941af0
chinzillla <96362668+Chinzillla@users.noreply.github.com>
2026-09-09 12:23:13 -0400

    Added Provecta S-Pan Setup Script and updated vistapano 2.0 setup script

--- Provecta S-Pan Setup.bat
+++ Provecta S-Pan Setup.bat
@@ -10,16 +10,17 @@ if errorlevel 1 (
 set "LAYERS=HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers"
 
 reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Pano\VAKCAP.exe" /t REG_SZ /d "~ RUNASADMIN" /f
-reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Pano\VAPAN_FET.exe" /t REG_SZ /d "~ RUNASADMIN" /f
-reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Pano\VAPAN_FET_S.exe" /t REG_SZ /d "~ RUNASADMIN" /f
+reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Pano\VAPAN_ETH.exe" /t REG_SZ /d "~ RUNASADMIN" /f
+reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Pano\VAPAN_ETH_S.exe" /t REG_SZ /d "~ RUNASADMIN" /f
+reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Pano\VAPAN_PLX_S.exe" /t REG_SZ /d "~ RUNASADMIN" /f
 
 reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Ceph_Fast\VAKCAP.exe" /t REG_SZ /d "~ RUNASADMIN" /f
-reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Ceph_Fast\VAPAN_FET.exe" /t REG_SZ /d "~ RUNASADMIN" /f
-reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Ceph_Fast\VAPAN_FET_S.exe" /t REG_SZ /d "~ RUNASADMIN" /f
+reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Ceph_Fast\VAPAN_ETH.exe" /t REG_SZ /d "~ RUNASADMIN" /f
+reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Ceph_Fast\VAPAN_ETH_S.exe" /t REG_SZ /d "~ RUNASADMIN" /f
 
 reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Ceph_Norm\VAKCAP.exe" /t REG_SZ /d "~ RUNASADMIN" /f
-reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Ceph_Norm\VAPAN_FET.exe" /t REG_SZ /d "~ RUNASADMIN" /f
-reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Ceph_Norm\VAPAN_FET_S.exe" /t REG_SZ /d "~ RUNASADMIN" /f
+reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Ceph_Norm\VAPAN_ETH.exe" /t REG_SZ /d "~ RUNASADMIN" /f
+reg.exe add "%LAYERS%" /v "C:\ProgramData\Duerr\VistaPano\Acquisition\Ceph_Norm\VAPAN_ETH_S.exe" /t REG_SZ /d "~ RUNASADMIN" /f
 
 reg.exe add "%LAYERS%" /v "C:\Program Files (x86)\Duerr\VistaPano\exe\VistaPano.exe" /t REG_SZ /d "~ RUNASADMIN" /f
 
@@ -27,9 +28,6 @@ icacls "C:\Program Files (x86)\Duerr" /grant:r "*S-1-1-0:(OI)(CI)F" /T /C
 icacls "C:\ProgramData\Duerr" /grant:r "*S-1-1-0:(OI)(CI)F" /T /C
 icacls "C:\Program Files\Duerr" /grant:r "*S-1-1-0:(OI)(CI)F" /T /C
 icacls "C:\VistaSoftData" /grant:r "*S-1-1-0:(OI)(CI)F" /T /C
-
-netsh advfirewall firewall add rule name="VistaPano 2.0" dir=in action=allow protocol=TCP localport=54466,31175 profile=domain,private enable=yes
-netsh advfirewall firewall add rule name="VistaPano 2.0" dir=out action=allow protocol=TCP remoteport=54466,31175 profile=domain,private enable=yes
 
 reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
 
